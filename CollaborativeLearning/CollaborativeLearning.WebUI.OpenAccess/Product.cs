7747c02542b1dd68cbb2994ee3842c63262defff
Yakup Adaklı <yakup.adakli@hotmail.com>
2014-01-25 19:17:59 +0200

    WebUI added.

---
+++ CollaborativeLearning/CollaborativeLearning.WebUI.OpenAccess/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollaborativeLearning.WebUI.OpenAccess
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
