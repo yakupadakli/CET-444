f45d8c6c60cb730f2c951bbb984fa52200a19acb
Ozan <mosindi@gmail.com>
2014-02-19 01:41:07 +0200

    Resource Complete File Upload Start

---
+++ CollaborativeLearning/CollaborativeLearning.WebUI/Content/FileUpload/Scripts/main.js
@@ -0,0 +1,17 @@
+/*
+ * jQuery File Upload Plugin JS Example 7.0
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/MIT
+ */
+
+/*jslint nomen: true, unparam: true, regexp: true */
+/*global $, window, document */
+
+
+
+
--- CollaborativeLearning/CollaborativeLearning.WebUI/Controllers/ResourceController.cs
+++ CollaborativeLearning/CollaborativeLearning.WebUI/Controllers/ResourceController.cs
@@ -91,6 +91,29 @@ namespace CollaborativeLearning.WebUI.Controllers
         }
 
         public ActionResult Delete(int id) {
+            Resource re = unitOfWork.ResourceListRepository.GetByID(id);
+
+            if (re != null)
+            {
+                try
+                {
+                    unitOfWork = new UnitOfWork();
+                    unitOfWork.ResourceListRepository.Delete(id);
+                    unitOfWork.Save();
+                }
+                catch (Exception ex)
+                {
+                    
+                    ModelState.AddModelError("",ex);
+                    ViewBag.ErrorType = "ResourceAdd";
+                    ViewBag.Message = "The system error. It can be database connection problem or anythin about server-side! Please try again. If this situation continue, Ask your system administrator."
+                        + " Detail Error: " + ex.Message;
+                };
+            }else{
+                 ModelState.AddModelError("","The Resource cannot find to delete");
+                    ViewBag.ErrorType = "ResourceAdd";
+                    ViewBag.Message = "The Resource cannot find to delete";
+            }
             return RedirectToAction("_PartialResourceList");
         }
         public ActionResult ChangeActiveStatus(int id, string Active)
@@ -115,6 +138,30 @@ namespace CollaborativeLearning.WebUI.Controllers
             var Model = unitOfWork.ResourceListRepository.Get().ToList();
 
             return PartialView(Model);
+        }
+        public ActionResult _PartialEditResource(int id) {
+            Resource model = new Resource();
+            Resource m = unitOfWork.ResourceListRepository.GetByID(id);
+            if (m != null)
+            {
+                model = m;
+                List<SelectListItem> ResourceCategory = new List<SelectListItem>();
+                ResourceCategory.Add(new SelectListItem { Text = "Select ResourceType", Value = "" });
+                ResourceCategory.Add(new SelectListItem { Text = "Text", Value = "Text" });
+                ResourceCategory.Add(new SelectListItem { Text = "Link", Value = "URL" });
+                ResourceCategory.Add(new SelectListItem { Text = "File/PDF", Value = "File/PDF" });
+                ResourceCategory.Add(new SelectListItem { Text = "File/Zip", Value = "File/Zip" });
+                ResourceCategory.Add(new SelectListItem { Text = "File/Document", Value = "File/Document" });
+                ResourceCategory.Add(new SelectListItem { Text = "File/Other", Value = "File/Other" });
+                ResourceCategory.Add(new SelectListItem { Text = "Other", Value = "Other" });
+
+                ViewBag.ResourceCategory = ResourceCategory;
+                return PartialView("_PartialEditResource", model);
+
+            }
+            else {
+                return null;
+            }
         }
     }
 }
