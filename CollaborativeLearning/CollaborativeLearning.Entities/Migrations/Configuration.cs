9f5ca59e5056130d0a344e63b237746bd926e149
Ozan <mosindi@gmail.com>
2014-02-25 16:20:38 +0200

    Sunum

--- CollaborativeLearning/CollaborativeLearning.Entities/Migrations/Configuration.cs
+++ CollaborativeLearning/CollaborativeLearning.Entities/Migrations/Configuration.cs
@@ -1,10 +1,11 @@
+
 namespace CollaborativeLearning.Entities.Migrations
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
-
+    
     internal sealed class Configuration : DbMigrationsConfiguration<CollaborativeLearning.Entities.DataContext>
     {
         public Configuration()
@@ -14,6 +15,37 @@ namespace CollaborativeLearning.Entities.Migrations
 
         protected override void Seed(CollaborativeLearning.Entities.DataContext context)
         {
+
+            context.Users.AddOrUpdate
+                (
+                new CollaborativeLearning.Entities.User {
+                    Username = "Admin",
+                    FirstName = "Hamdi",
+                    LastName = "Erkunt",
+                    Email = "erkunt@boun.edu.tr",
+                    PhoneNumber = "05009990909",
+                    Gender = "Male",
+                    IsApproved = true,
+                    IsLockedOut = false,
+                    LastLockoutDate = DateTime.UtcNow,
+                    LastActivityDate = DateTime.UtcNow,
+                    LastPasswordChangedDate = DateTime.UtcNow,
+                    LastPasswordFailureDate = DateTime.UtcNow,
+                    Password = CollaborativeLearning.WebUI.Membership.Crypto.HashPassword("123456"),
+                    CreateDate = DateTime.UtcNow,
+                    PasswordFailuresSinceLastSuccess = 0,
+                    RoleID = 1
+                }
+                );
+            context.Roles.AddOrUpdate(
+                new CollaborativeLearning.Entities.Role { RoleName = "Instructor", Description = "Teacher" },
+                new CollaborativeLearning.Entities.Role { RoleName = "Mentor", Description = "Assistant" },
+                new CollaborativeLearning.Entities.Role { RoleName = "Student", Description = "Default User Type" }
+
+            );
+
+             
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
