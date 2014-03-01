a7d94f867a9ce0e1071b48da5fbd81e4ecacf5a8
Ozan <mosindi@gmail.com>
2014-03-01 23:23:47 +0200

    Submit Work Start

---
+++ CollaborativeLearning/CollaborativeLearning.WebUI/Controllers/GroupWorkController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CollaborativeLearning.Entities;
+using CollaborativeLearning.DataAccess;
+
+namespace CollaborativeLearning.WebUI.Controllers
+{
+    public class GroupWorkController : Controller
+    {
+        UnitOfWork unitOfWork = new UnitOfWork();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _PartialSubmitWork(int id,int groupID) {
+            unitOfWork = new UnitOfWork();
+            WorkSemesterDueDate wsd = unitOfWork.WorkSemesterDueDateRepository.GetByID(id);
+            GroupWork model = new GroupWork();
+            if (wsd !=null)
+            {
+                model.WorkId = wsd.WorkID;
+                model.GroupID = groupID;
+            }
+            return PartialView(model);
+           
+        }
+    }
+}
