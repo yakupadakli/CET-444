0ae2f0ee4ae34598a024544dea980a12ae98ad46
Yakup Adaklı <yakup.adakli@hotmail.com>
2014-03-02 15:33:37 +0200

    GroupMeetings starts.

---
+++ CollaborativeLearning/CollaborativeLearning.WebUI/Controllers/GroupMeetingsController.cs
@@ -0,0 +1,22 @@
+﻿using CollaborativeLearning.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollaborativeLearning.WebUI.Controllers
+{
+    public class GroupMeetingsController : Controller
+    {
+        //
+        // GET: /GroupMeetings/
+
+        public ActionResult Index()
+        {
+            Group currentGroup = null;
+            return View();
+        }
+
+    }
+}
