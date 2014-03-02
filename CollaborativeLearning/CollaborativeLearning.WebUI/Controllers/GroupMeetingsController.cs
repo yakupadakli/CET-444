f300953581e02678e51c55a06fd8da1c5ed59694
Oğuzhan Kemal <veriadresi@hotmail.com>
2014-03-02 18:18:47 +0200

    Resim Upload tamam

--- CollaborativeLearning/CollaborativeLearning.WebUI/Controllers/GroupMeetingsController.cs
+++ CollaborativeLearning/CollaborativeLearning.WebUI/Controllers/GroupMeetingsController.cs
@@ -1,6 +1,8 @@
-﻿using CollaborativeLearning.Entities;
+﻿using CollaborativeLearning.DataAccess;
+using CollaborativeLearning.Entities;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,13 +11,137 @@ namespace CollaborativeLearning.WebUI.Controllers
 {
     public class GroupMeetingsController : Controller
     {
-        //
-        // GET: /GroupMeetings/
-
-        public ActionResult Index()
+        UnitOfWork unitOfWork = new UnitOfWork();       
+        public ActionResult Index(int SemesterID, int? groupId)
         {
-            Group currentGroup = null;
+            if (SemesterID != null)
+                ViewBag.SemesterID = SemesterID;
+            else
+                ViewBag.SemesterID = 0;
+
+            if (groupId != null)
+                ViewBag.groupId = groupId;
+            else
+                ViewBag.groupId = 0;
+
             return View();
+        }
+
+        public ActionResult _AddShowMeetingNotes(int SemesterID, int groupId)
+        {
+            try
+            {
+                if (SemesterID != 0)
+                {
+                    Group group = null;
+                    if (groupId == 0)
+                    {
+                        group = HelperController.GetCurrentUser().Groups.Where(g => g.SemesterID == SemesterID).FirstOrDefault();
+                    }
+                    else
+                    {
+                        group = unitOfWork.GroupRepository.GetByID(groupId);
+                    }
+
+                    if (group != null)
+                    {
+                        return PartialView(group);
+                    }
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        // Return add meeting note partial
+        public ActionResult _AddNewMeeting(int groupId)
+        {
+            if (groupId != 0)
+            {
+                ViewBag.groupId = groupId;
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult _AddNewMeeting(MeetingNote model, int groupId, HttpPostedFileBase filePicture, HttpPostedFileBase fileRar)
+        {
+            Group group = unitOfWork.GroupRepository.GetByID(groupId);
+            model.GroupID = groupId;
+            model.regDate = DateTime.Now;
+            model.regUserID = HelperController.GetCurrentUser().Id;
+
+            if (ModelState.IsValid)
+            {
+                unitOfWork.MeetingNoteRepository.Insert(model);
+                unitOfWork.Save();
+
+                if (filePicture != null)
+                {
+                    unitOfWork = new UnitOfWork();
+
+                    MeetingNote meetingNote = unitOfWork.MeetingNoteRepository.GetByID(model.Id);
+
+                    string directoryPath = Path.Combine(Server.MapPath("~/"),"GroupMeetings","MeetingPhotos", meetingNote.Group.Semester.CourseName);
+                    if (!Directory.Exists(directoryPath))
+                    {
+                        Directory.CreateDirectory(directoryPath);
+                    }
+                    string[] file = filePicture.FileName.Split('.');
+                    string uzanti = file[1];
+                    var fileName = meetingNote.Id.ToString() + "." + uzanti;
+                    var fileNameEncoded = fileName.ToString();
+                    if (fileName != null)
+                    {
+                        var fullPath = Path.Combine(directoryPath, fileNameEncoded);
+                        try
+                        {
+                            MeetingNoteFile meetingNoteFile = new MeetingNoteFile();
+                            meetingNoteFile.MeetingNoteID = meetingNote.Id;
+                            meetingNoteFile.FileName = fileNameEncoded;
+                            meetingNoteFile.FileSize = filePicture.ContentLength;
+                            meetingNoteFile.FileType = filePicture.ContentType;
+                            meetingNoteFile.FileUrl = Path.Combine(fileNameEncoded);
+                            meetingNoteFile.regDate = DateTime.Now;
+                            meetingNoteFile.regUserID = HelperController.GetCurrentUserId();
+
+
+                            unitOfWork = new UnitOfWork();
+                            unitOfWork.MeetingNoteFileRepository.Insert(meetingNoteFile);
+                            unitOfWork.Save();
+                            filePicture.SaveAs(fullPath);
+
+
+                        }
+                        catch (Exception ex)
+                        {
+                            ModelState.AddModelError("", ex);
+                            ViewBag.ErrorType = "ResourceAdd";
+                            ViewBag.Message = "The system error. It can be database connection problem or anythin about server-side! Please try again. If this situation continue, Ask your system administrator."
+                                + " Detail Error: " + ex.Message;
+                        }
+                    }
+                }
+
+                int roleId = HelperController.GetCurrentUser().RoleID;
+                if (roleId == 3)
+                {
+                    groupId = 0;
+                    return RedirectToAction("_AddShowMeetingNotes", new { group.SemesterID, groupId });
+                }
+                else
+                    return RedirectToAction("_AddShowMeetingNotes", new { group.SemesterID, groupId });
+
+
+            }
+            return PartialView(model);
         }
 
     }
